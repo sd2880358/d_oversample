4ea30a32dbbd2254c6f99c58cb8c1c71f519a8c4
sd2880358 <leedonhom@gmail.com>
2021-09-20 10:39:06 -0400

    first

--- dataset.py
+++ dataset.py
@@ -51,9 +51,9 @@ class Dataset():
     self.batch_size = batch_size
     self.dataset = dataset
     self.switcher = {
-      'mnist': np.load('../dataset/mnist_dataset1.npz'),
-      'celebA': np.load('../dataset/celebA_dataset.npz'),
-      'large_celebA': np.load('../dataset/celebA_large_dataset.npz'),
+      'mnist': np.load('./dataset/mnist_dataset1.npz'),
+      'celebA': np.load('./dataset/celebA_dataset.npz'),
+      'large_celebA': np.load('./dataset/celebA_large_dataset.npz'),
       #'fashion_mnist': np.load('../dataset/fashion_mnist.npz')
     }
 
